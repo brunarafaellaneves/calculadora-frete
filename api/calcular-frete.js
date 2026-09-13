d927611be11162dfa1066f971478b0173f85627d
Bruna R. Neves <brunarafaellaneves@gmail.com>
2026-09-13 01:02:02 -0300

    Update calcular-frete.js

--- api/calcular-frete.js
+++ api/calcular-frete.js
@@ -1,4 +1,3 @@
-```javascript
 export default async function handler(req, res) {
 
     if (req.method !== "POST") {
@@ -140,4 +139,3 @@ export default async function handler(req, res) {
     }
 
 }
-```
